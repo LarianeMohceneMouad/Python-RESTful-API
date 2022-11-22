e5f38f215061eece16b8e73f5e3213f8df70d069
Mohcene Mouad Lariane <76631615+LarianeMohceneMouad@users.noreply.github.com>
2022-11-22 23:37:45 +0100

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-account_sid = 'ACe58d0227656011866a5527f83cc39233'
-auth_token = 'e413069a355825b2b375446b7f1ef952'
+account_sid = '******************************'
+auth_token = '******************************'
+twilio_number = '**************'
 
-twilio_number = '+13467066909'
-target_number = '+213793445924'
+# Use your own twilio tokens
